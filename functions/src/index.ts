21dad47d5b6384f3a0482e27bb214172eeef6ae3
LeoAdamek <leo@adamek.me>
2022-08-15 18:35:27 +0100

    add a login func

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,17 +1,7 @@
 import * as functions from "firebase-functions";
-import { initializeApp } from "firebase-admin";
-import * as _ from 'lodash';
 
-const app = initializeApp();
-
-export const generateLap = functions.https.onRequest(async (req, res) => {
-})
-
-
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const loginHandler = functions.auth.user().beforeSignIn(
+    (user, _context) => {
+      functions.logger.info(`Checking additional auth data for ${user.uid}`);
+    }
+);
